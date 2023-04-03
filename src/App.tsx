7428599336a34e1bc46aa11cace4f330f11db4d0
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-03 12:22:00 +0200

    conjvre

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <p>Test</p>
+        </div>
+    );
+}
+
+export default App;
