37020d65e14df86fb575c5532083c066297394ef
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-05 10:29:44 +0200

    rough resume styling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -26,44 +26,46 @@ export default class Resume extends Component {
                             {this.props.data.email}
                         </h3>
                     </div>
-                    <div className="resume--education-container">
-                        <div className="resume--university-container">
-                            <h3 className="resume--university">
-                                {this.props.data.university}
-                            </h3>
-                            <h3 className="resume--degree">
-                                {this.props.data.degree}
-                            </h3>
-                        </div>
-                        <div className="resume--uni-dates-container">
-                            <h3 className="resume--uni-start-date">
-                                {this.props.data.uniStartDate}
-                            </h3>
-                            <h3 className="resume--uni-end-date">
-                                {this.props.data.uniEndDate}
-                            </h3>
-                        </div>
+                </div>
+                <h2 className="resume--education-title">Education</h2>
+                <div className="resume--education-container">
+                    <div className="resume--university-container">
+                        <h3 className="resume--university">
+                            {this.props.data.university}
+                        </h3>
+                        <h3 className="resume--degree">
+                            {this.props.data.degree}
+                        </h3>
                     </div>
-                    <div className="resume--experience-container">
-                        <div className="resume--work-container">
-                            <h3 className="resume--position">
-                                {this.props.data.position}
-                            </h3>
-                            <h3 className="resume--company">
-                                {this.props.data.company}
-                            </h3>
-                            <h3 className="resume--notes">
-                                {this.props.data.notes}
-                            </h3>
-                        </div>
-                        <div className="resume--exp-dates-container">
-                            <h3 className="resume--exp-start-date">
-                                {this.props.data.expStartDate}
-                            </h3>
-                            <h3 className="resume--exp-end-date">
-                                {this.props.data.expEndDate}
-                            </h3>
-                        </div>
+                    <div className="resume--uni-dates-container">
+                        <h3 className="resume--uni-start-date">
+                            {this.props.data.uniStartDate}
+                        </h3>
+                        <h3 className="resume--uni-end-date">
+                            {this.props.data.uniEndDate}
+                        </h3>
+                    </div>
+                </div>
+                <h2 className="resume--experience-title">Experience</h2>
+                <div className="resume--experience-container">
+                    <div className="resume--work-container">
+                        <h3 className="resume--position">
+                            {this.props.data.position}
+                        </h3>
+                        <h3 className="resume--company">
+                            {this.props.data.company}
+                        </h3>
+                        <h3 className="resume--notes">
+                            {this.props.data.notes}
+                        </h3>
+                    </div>
+                    <div className="resume--exp-dates-container">
+                        <h3 className="resume--exp-start-date">
+                            {this.props.data.expStartDate}
+                        </h3>
+                        <h3 className="resume--exp-end-date">
+                            {this.props.data.expEndDate}
+                        </h3>
                     </div>
                 </div>
             </div>
