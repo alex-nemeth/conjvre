6ffc75af3dcd5c380f1b14bca27e1cf91568a4c4
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-05 10:07:19 +0200

    data gen on last page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,27 +1,47 @@
 import React, { Component } from "react";
 
 export default class Experience extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="experience--container">
                 <h2 className="experience--title">Experience: </h2>
                 <input
                     type="text"
-                    className="experience--school"
+                    name="position"
+                    className="experience--position"
                     placeholder="Position"
+                    onChange={this.props.handleChange}
                 ></input>
                 <input
                     type="text"
-                    className="experience--field"
+                    name="company"
+                    className="experience--company"
                     placeholder="Company Name"
+                    onChange={this.props.handleChange}
                 ></input>
-                <input type="date" className="experience--date-start"></input>
-                <input type="date" className="experience--date-end"></input>
+                <input
+                    type="date"
+                    className="experience--date-start"
+                    name="expStartDate"
+                    onChange={this.props.handleChange}
+                ></input>
+                <input
+                    type="date"
+                    className="experience--date-end"
+                    name="expEndDate"
+                    onChange={this.props.handleChange}
+                ></input>
                 <br />
                 <textarea
                     type="text"
+                    name="notes"
                     className="experience--text"
                     placeholder="Notes"
+                    onChange={this.props.handleChange}
                 ></textarea>
             </div>
         );
